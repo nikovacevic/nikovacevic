a596b6b39c0d1dc14f9c3f0a2ecc16de3659ec04
Niko Kovacevic <nikovacevic@gmail.com>
2017-04-24 10:35:12 -0600

    Initial commit

---
+++ public/js/ui.js
@@ -0,0 +1,24 @@
+if (!UI) {
+  var UI = {};
+}
+
+(function () {
+  // Functions
+
+  // toggleMenu toggles display of dropdown list elements in the given menu
+  UI.toggleMenu = function (menu) {
+    var lis = menu.getElementsByClassName("dropdown-li");
+    Array.prototype.forEach.call(lis, function (li) {
+      li.classList.toggle("hidden");
+    });
+  }
+
+  // Events
+
+  // On click of dropdown button, toggle dropdown menu display
+  var btn = document.getElementById("dropdown-button");
+  var menu = document.getElementById("dropdown-menu");
+  btn.onclick = function() {
+    UI.toggleMenu(menu);
+  };
+})();
