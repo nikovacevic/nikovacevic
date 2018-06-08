98f07d95bca156a640d98068c8786988e711194e
Niko Kovacevic <nikovacevic@gmail.com>
2018-06-07 18:30:52 -0600

    Moved CDN files to locally-served. Fade-in loading. JS overload of VH.

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,17 @@
 $(function () {
   UI = {};
+  UI.viewportHeight = $(window).height();
+  UI.viewportWidth = $(window).width();
+
+  UI.init = function () {
+    // ATF
+    $("section#atf").css('height', UI.viewportHeight-$('header').height());
+    $("section#atf").css('margin-top', $('header').height());
+    setTimeout(function () {
+      $("body").removeClass('loading');
+    }, 250)
+  }
+
   UI.toggleFullScreenNav = function (nav) {
     nav.toggleClass('visible');
     $("header>nav").toggleClass('scrolled', nav.hasClass('visible'));
@@ -8,4 +20,6 @@ $(function () {
     var nav = $("nav#full-screen-nav");
     UI.toggleFullScreenNav(nav);
   });
+
+  UI.init();
 });
